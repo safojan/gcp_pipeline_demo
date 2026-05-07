57f935bd1ba443ae40b6fcdf31a75c4206d0f8b9
safojan <jsafdar199@gmail.com>
2026-05-07 11:48:04 +0500

    replace artifect repo url 8

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ async function initDatabase() {
       dbConn = await mysql.createPool({
         user: dbSecret.username,
         password: dbSecret.password,
-        socketPath: '/cloudsql/699656816818:us-central1:gcp-demo-db',
+        socketPath: '/cloudsql/brandai-36abd:us-central1:gcp-demo-db:us-central1:gcp-demo-db',
         database: dbSecret.database,
         waitForConnections: true,
         connectionLimit: 10,
